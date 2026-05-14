114d5310d5e9435b09dd3af11886a1def4c0720c
langtang-MacM4 <langlang1991@gmail.com>
2026-05-14 15:45:45 +0800

    Initial Commit

---
+++ OodiPop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OodiPop
+//
+//  Created by 唐浪 on 2026.05.14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OodiPop/OodiPopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OodiPopApp.swift
+//  OodiPop
+//
+//  Created by 唐浪 on 2026.05.14.
+//
+
+import SwiftUI
+
+@main
+struct OodiPopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
